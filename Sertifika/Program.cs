ceda7c6613829267685ab5a4aed87a3cc0e3e5ab
ahmet özgüç <mona.noyan@gmail.com>
2026-03-06 01:09:20 +0300

    publish

--- Sertifika/Program.cs
+++ Sertifika/Program.cs
@@ -54,6 +54,18 @@ builder.Services.AddAuthentication(options =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
         RoleClaimType = System.Security.Claims.ClaimTypes.Role
     };
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
 });
 
 builder.Services.AddAuthorization();
